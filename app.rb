4297e7522d8b2777134cacc28dcb1860e1973fc8
gabrielagsole <ggarciasole@gmail.com>
2018-08-14 17:18:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,4 +4,10 @@ class App < Sinatra::Base
   get '/name' do
     "Hello my name is Gabi"
   end 
+  get '/hometown' do
+    "My hometown is Barcelona"
+  end 
+  get '/favorite-song' do
+    "I don't have a favorite song"
+  end 
 end
